b44e3f22e09f7e6e716d1721039f9ef3d2793a8a
kazaoes <cassiem.davids316@gmail.com>
2026-04-29 04:23:56 -0400

    second commit

--- 0-authentication.tf
+++ 0-authentication.tf
@@ -4,8 +4,3 @@ provider "google" {
   region      = "us-east1"
   credentials = "kazapes1-d5061cf53f9a.json"
 }
-
-# I'm doing this tonight while prepping my students for their sessions with Lizzo
-
-# comment there will be no diddy oil bootygasms here
-# Lizzo will be pegging Carlton and inducing a bootygasm deep in him with Chewbacca watching!
